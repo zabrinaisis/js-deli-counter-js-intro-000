fca4d56ea0b048f731e89a5566e955335759335a
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-17 18:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ function takeANumber(line, name) {
   line.push(name);
   var num = line.length + 1;
   console.log(`Welcome ${name}, you are number ${num} in line.`);
+  return (line);
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    console.log("There is nobody waiting to be served!");
+  } else {
+    return (line.shift());
+  }
+}
+
